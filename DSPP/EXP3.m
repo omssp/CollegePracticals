33f81756da8fea25439ad24800ce96647e97d10d
Shardul Pakhale <omssp@localhost.localdomain>
2019-07-19 15:36:26 +0530

    Added EXP3 of DSP

---
+++ DSPP/EXP3.m
@@ -0,0 +1,29 @@
+clc
+clear all
+close all
+
+na = input('Enter the  amount to be delayed : ')
+nd = input('Enter the amount to be advanced : ')
+
+xl = input('Enter the  left limit : ')
+xr = input('Enter the right limit : ')
+
+x = xl : 1 : xr
+
+y = input('Enter the Sequence : ')
+
+subplot( 3, 1, 1 )
+stem( x, y )
+title('Signal x(n)')
+
+xa = x + na
+
+subplot( 3, 1, 2 )
+stem( xa, y )
+title(['Advanced Signal x(n + ' (na + 48) ')'])
+
+xd = x - nd
+
+subplot( 3, 1, 3 )
+stem( xd, y )
+title(['Delayed Signal x(n - ', (nd + 48), ')'])
