0bc53eedbbdc2330e176ada8af198eaf35d3079c
Shardul Pakhale <omssp@localhost.localdomain>
2019-08-22 10:02:35 +0530

    Added DAA EXP6

---
+++ DAAP/EXP6.cpp
@@ -0,0 +1,105 @@
+#include "iostream"
+#include "list"
+
+class Graph {
+private:
+	int vn;
+	std :: list<int> *adj;
+public:
+	Graph(int vn) {
+		this->vn = vn;
+		adj = new std :: list<int>[vn];
+	}
+	void addEdge( int v, int w ) {
+		adj[v].push_back(w);
+	}
+	void BFS( int source ) {
+		bool *visited = new bool[vn];
+		for ( int i = 0; i < vn; i++ )
+			visited[i] = false;
+		
+		std :: list<int> queue;
+
+		visited[source] = true;
+		queue.push_back(source);
+
+		std :: list<int> :: iterator i;
+
+		std :: cout << "\n BFS : [ ";
+
+		while (!queue.empty()) {
+			source = queue.front();
+			std :: cout << source << ", ";
+			queue.pop_front();
+			for ( i = adj[source].begin(); i != adj[source].end(); ++i ) {
+				if ( !visited[*i] ) {
+					visited[*i] = true;
+					queue.push_back(*i);
+				}
+			}
+		}
+		std :: cout << "\b\b ]";
+	}
+
+	void DFSUtil(int v, bool visited[]) {
+		visited[v] = true;
+		std :: cout << v << ", ";
+
+		std :: list<int>::iterator i;
+		for ( i = adj[v].begin(); i != adj[v].end(); ++i ) {
+			if ( !visited[*i] )
+				DFSUtil(*i, visited);
+		}
+	}
+
+	void DFS( int v ) {
+		bool *visited = new bool[vn];
+		for (int i = 0; i < vn; ++i) {
+			visited[i] = false;
+		}
+		std :: cout << "\n\n DFS : [ ";
+		DFSUtil(v, visited);
+		std :: cout << "\b\b ]";
+	}
+
+	void input() {
+		int n;
+		std :: cout << "\nEnter the total number of Edges : ";
+		std :: cin >> n;
+		for (int i = 0; i < n; ++i) {
+			int s, d;
+			std :: cout << "Enter Edge " << (i + 1) << " [S D] : ";
+			std :: cin >> s >> d;
+			addEdge(s, d);
+		}
+	}
+
+	/*void input() {
+		std :: cout << "\nEnter the Edges\n\n";
+		for (int i = 0; i < vn; ++i) {
+			for (int j = 0; j < vn; ++j) {
+				char ch;
+				std :: cout << " Edge [" << i << ", " << j << "] Present(y/n)? ";
+				std :: cin >> ch;
+				if ( ch == 'y' || ch == 'Y' )
+					addEdge(i, j);
+			}
+		}
+	}*/
+};
+int main() {
+	int n;
+	std :: cout << "Enter the number of verices : ";
+	std :: cin >> n;
+
+	Graph a(n);
+	a.input();
+
+	std :: cout << "\nEnter the source vertex number : ";
+	std :: cin >> n;
+
+	a.BFS(n);
+	a.DFS(n);
+
+	return 0;
+}
